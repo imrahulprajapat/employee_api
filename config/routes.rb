bf0301d81aae7be3d07a7a9eb3fca6364fd84f03
imrahulprajapat <rahul.prajapat7921@gmail.com>
2026-03-20 17:22:38 +0000

    added salary metrics

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   resources :employees do
-    get :salary
+    get :salary, on: :member
+    get :country_metrics, on: :collection
+    get :job_metrics, on: :collection
   end
 end
